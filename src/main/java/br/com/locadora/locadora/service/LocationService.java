8614302b4dbae8b7f13cd6859ebaec2bf4e55399
GJAlves <@Allvs1234>
2020-08-16 12:30:45 -0300

    Ajuste em versão do Java

--- src/main/java/br/com/locadora/locadora/service/LocationService.java
+++ src/main/java/br/com/locadora/locadora/service/LocationService.java
@@ -2,9 +2,7 @@ package br.com.locadora.locadora.service;
 
 import br.com.locadora.locadora.model.Location;
 import br.com.locadora.locadora.model.Movie;
-import br.com.locadora.locadora.model.User;
 import br.com.locadora.locadora.repository.LocationRepository;
-import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
